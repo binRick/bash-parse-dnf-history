addcc522ea3a9207f6d8edbd50b783fb8111d8eb
root <admin@vpntech.net>
2020-01-18 18:51:23 -0500

    improve python parsing script

---
+++ dev-py-jq.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+
+nodemon --delay 1 -w . -e py,sh,yaml,json -x sh -c './parse-dnf.py|jq'
--- parse-dnf.py
+++ parse-dnf.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 import os, subprocess, json, sys
 
+DNF_TRANSACTIONS = {}
 DNF_HISTORY_CMD='sudo -u root dnf history list'
+DNF_HISTORY_INFO_CMD='sudo -u root dnf history info'
 DNF_HISTORY_TIMEOUT=10
 
 EXEC = 'timeout {} {}'.format(
         DNF_HISTORY_TIMEOUT,
         DNF_HISTORY_CMD,
        ).split(' ')
-print(' '.join(EXEC))
 
 P = subprocess.Popen(EXEC, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-PID = P.pid
+pid = P.pid
 try:
     out, err = P.communicate(timeout=DNF_HISTORY_TIMEOUT+5)
 except TimeoutExpired:
@@ -19,6 +20,7 @@ except TimeoutExpired:
     out, err = P.communicate()
     
 code = P.returncode
+
 
 outlines = []
 for l in out.decode().strip().splitlines():
@@ -29,18 +31,37 @@ for l in out.decode().strip().splitlines():
         for L in l.split('|'):
             L = L.strip()
             PS.append(L)
-        #print('PS={}'.format(PS))
         PSO = {
          'id': PS[0],
          'args': PS[1],
          'date': PS[2],
          'actions': PS[3],
          'altered': PS[4],
+         'info_cmd': '{} {}'.format(
+            DNF_HISTORY_INFO_CMD,
+            PS[0],
+          ).split(' '),
         }
-        print(PSO)
+        IP = subprocess.Popen(PSO['info_cmd'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        try:
+            ip_out, ip_err = IP.communicate(timeout=10)
+        except:
+            IP.kill()
+            ip_out, ip_err = IP.communicate()
+
+        PSO['info_lines'] = ip_out.decode().strip().splitlines()
+        
+        for _l in PSO['info_lines']:
+            if _l.startswith('Return-Code '):
+                PSO['info_return_code'] = _l.split(':')[1].strip()
+            if _l.startswith('User '):
+                PSO['info_user'] = _l.split(':')[1].strip().split(' ')[0]
+
+        DNF_TRANSACTIONS[PSO['id']] = PSO
 
 err = err.decode().strip().splitlines()
 
-print('code={}'.format(code))
-#print('outlines={}'.format(outlines))
-#print('err={}'.format(err))
+DNF_REPORT = {
+    'transactions': DNF_TRANSACTIONS,
+}
+print(json.dumps(DNF_REPORT))
